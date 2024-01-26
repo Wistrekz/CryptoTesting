e67f9a7fe0ee93263b3563d4b3f562d793db93e9
Wistrekz <96283348+Wistrekz@users.noreply.github.com>
2024-01-27 01:15:01 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.File_check = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Byte_Generate = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // File_check
+            // 
+            this.File_check.Location = new System.Drawing.Point(482, 52);
+            this.File_check.Name = "File_check";
+            this.File_check.Size = new System.Drawing.Size(48, 20);
+            this.File_check.TabIndex = 0;
+            this.File_check.Text = "...";
+            this.File_check.UseVisualStyleBackColor = true;
+            this.File_check.Click += new System.EventHandler(this.File_check_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(252, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(224, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Byte_Generate
+            // 
+            this.Byte_Generate.Location = new System.Drawing.Point(267, 137);
+            this.Byte_Generate.Name = "Byte_Generate";
+            this.Byte_Generate.Size = new System.Drawing.Size(209, 23);
+            this.Byte_Generate.TabIndex = 2;
+            this.Byte_Generate.Text = "Сгенерировать байтовый вывод";
+            this.Byte_Generate.UseVisualStyleBackColor = true;
+            this.Byte_Generate.Click += new System.EventHandler(this.Byte_Generate_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(166, 187);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(430, 91);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(166, 333);
+            this.progressBar1.Maximum = 4;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(430, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(342, 388);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Готово";
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Byte_Generate);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.File_check);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button File_check;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Byte_Generate;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void File_check_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();    //Открытие и настройка поля для выбора файла
+            openFileDialog1.Title = "Select File";
+            openFileDialog1.InitialDirectory = @"C:\";//--"C:\\";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName != "")
+            { 
+                textBox1.Text = openFileDialog1.FileName; 
+            }
+            else
+            {
+                textBox1.Text = "";
+            }
+        }
+
+        private void Byte_Generate_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(textBox1.Text)) //Проверка на существование файла
+            {
+                FileStream Workfile = new FileStream(textBox1.Text, FileMode.Open);   //
+                long LenBytes = Workfile.Length;                                      //
+                double a = Convert.ToDouble(Workfile.Length) / 1024 / 1024;           // Открытие файла и подсчёт байтов для вывода         
+                int b = 0;                                                            //
+                if (Math.Round(a - 0.5) == Math.Round(a))                             //
+                {
+                    b = (int)Math.Round(a);
+                }
+                else
+                {
+                    b = (int)Math.Round(a) - 1;
+                }
+                if(a % Math.Round(a) == 1)
+                {
+                    progressBar1.Maximum = b;
+                }
+                else
+                {
+                    progressBar1.Maximum = b + 1;
+                }
+
+                progressBar1.Minimum = 0;                        //Настройка ProgressBar
+                progressBar1.Style = ProgressBarStyle.Blocks;
+                progressBar1.Value = 0;
+                progressBar1.Step = 1;
+                if(a < (12 * 1024) && a > (2 * 1024))      //Если больше 12 гб или меньше 2гб то не проходит
+                {
+                    byte[] BY = new byte[1024*1024];
+                    BinaryReader reader = new BinaryReader(Workfile, Encoding.GetEncoding(1251));     //Счёт байтов с поддержкой 
+                    
+                    for (int i = 0; i < b; i++)     //Циклом проходятся по всем гигабайтам
+                    {
+                        if (LenBytes >= BY.Length)
+                        {
+                            reader.Read(BY, 0, BY.Length);//Запись в массив гигабайт байтов
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        string[] a1 = HexDump(BY);        //Вывод в указанном в задании виде
+                        for (int q = 0; q < a1.Length; q++)
+                        {
+                            richTextBox1.Text += a1[q];
+                        }
+                        
+                        LenBytes -= BY.Length;           //Считает сколько осталось байтов
+                        Array.Clear(BY, 0, BY.Length);
+                        progressBar1.PerformStep();  //Продвигается ProgressBar
+                    }
+                    if(LenBytes > 0) //Оставшийся байты досчитываются
+                    {
+                        byte[] BY1 = new byte[LenBytes];
+                        reader.Read(BY1, 0, (int)LenBytes);
+                        string[] R = HexDump(BY);
+                        for (int i = 0; i < R.Length; i++)
+                        {
+                            richTextBox1.Text += R[i];
+                        }
+                        progressBar1.PerformStep();
+                    }
+                    reader.Close();
+                    Workfile.Close(); //Закрываются потоки файла
+                    label1.Visible = true;
+                }
+                else
+                {
+                    Workfile.Close();
+                    if (a < (2 * 1024))  //Если меньше 2гб то быстрая запись файла
+                    {
+                        byte[] buffer = File.ReadAllBytes(textBox1.Text);
+                        string[] A = HexDump(buffer);
+                        for (int i = 0; i < A.Length; i++)
+                        {
+                            richTextBox1.Text += A[i];
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Больше 12 гигов нельзя"); //Искуственное ограничение по размеру файла
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Такого файла не существует");  //Если пути нет такого
+            }
+        }
+
+        public static string[] HexDump(byte[] bytes, int bytesPerLine = 16)      
+        {
+            char[] HexChars = "0123456789ABCDEF".ToCharArray();
+
+            int firstHexColumn =
+                  8                   // 8 символов для адреса
+                + 3;                  // 3 пробела
+
+            int firstCharColumn = firstHexColumn
+                + bytesPerLine * 3       // - 2 цифры для шестнадцатеричного значения и 1 пробел
+                + (bytesPerLine - 1) / 8 // - 1 дополнительный пробел через каждые 8 символов, начиная с 9-го
+                + 2;                  // 2 пропуска
+
+            //int lineLength = firstCharColumn
+            //    + bytesPerLine           // - символы для отображения значения ascii
+            //    + Environment.NewLine.Length; // Возврат каретки и подача строки (обычно должно быть 2)
+
+            char[] line = (new String(' ', /*lineLength*/(firstCharColumn + bytesPerLine + Environment.NewLine.Length) - Environment.NewLine.Length) + Environment.NewLine).ToCharArray();
+            int expectedLines = (bytes.Length + bytesPerLine - 1) / bytesPerLine;
+            StringBuilder result = new StringBuilder((expectedLines * (firstCharColumn + bytesPerLine + Environment.NewLine.Length)) / 8);
+            int lineCount = 0;
+            string[] Y = new string[8];
+            for (int i = 0; i < bytes.Length; i += bytesPerLine)
+            {
+                
+                line[0] = HexChars[(i >> 28) & 0xF];
+                line[1] = HexChars[(i >> 24) & 0xF];
+                line[2] = HexChars[(i >> 20) & 0xF];
+                line[3] = HexChars[(i >> 16) & 0xF];
+                line[4] = HexChars[(i >> 12) & 0xF];
+                line[5] = HexChars[(i >> 8) & 0xF];
+                line[6] = HexChars[(i >> 4) & 0xF];
+                line[7] = HexChars[(i >> 0) & 0xF];
+
+                int hexColumn = firstHexColumn;
+                int charColumn = firstCharColumn;
+
+                for (int j = 0; j < bytesPerLine; j++)
+                {
+                    if (j > 0 && (j & 7) == 0) hexColumn++;
+                    if (i + j >= bytes.Length)
+                    {
+                        line[hexColumn] = ' ';
+                        line[hexColumn + 1] = ' ';
+                        line[charColumn] = ' ';
+                    }
+                    else
+                    {
+                        byte b = bytes[i + j];
+                        line[hexColumn] = HexChars[(b >> 4) & 0xF];
+                        line[hexColumn + 1] = HexChars[b & 0xF];
+                        line[charColumn] = (b < 32 ? '·' : (char)b);
+                    }
+                    hexColumn += 3;
+                    charColumn++;
+                }
+                if(result.Length < (expectedLines * (firstCharColumn + bytesPerLine + Environment.NewLine.Length)) / 8)
+                {
+                    result.Append(line);
+                }
+                else
+                {
+                    switch (lineCount)
+                    {
+                        case 0:
+                            Y[0] = result.ToString();
+                            break;
+                        case 1:
+                            Y[1] = result.ToString();
+                            break;
+                        case 2:
+                            Y[2] = result.ToString();
+                            break;
+                        case 3:
+                            Y[3] = result.ToString();
+                            break;
+                        case 4:
+                            Y[4] = result.ToString();
+                            break;
+                        case 5:
+                            Y[5] = result.ToString();
+                            break;
+                        case 6:
+                            Y[6] = result.ToString();
+                            break;
+                        case 7:
+                            Y[7] = result.ToString();
+                            break;
+                    }
+                    result.Remove(0, result.Length);
+                    lineCount++;
+
+                    result.Append(line);
+                }
+            }
+            return Y;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
